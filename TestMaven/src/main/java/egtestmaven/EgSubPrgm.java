d6215a94087d819ed28d0395e6ff72a57b41a9e4
arshapresannan <101731042+arshapresannan@users.noreply.github.com>
2022-03-18 11:48:21 +0530

    Uploading my project to github repository

---
+++ TestMaven/src/main/java/egtestmaven/EgSubPrgm.java
@@ -0,0 +1,13 @@
+package egtestmaven;
+
+public class EgSubPrgm {
+
+	public static void main(String[] args) {
+		 int a=50;
+		 int b=40;
+		 int c=a-b;
+		 System.out.println("Result is: "+c);
+
+	}
+
+}
